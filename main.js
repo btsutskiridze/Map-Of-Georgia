b27c0115e9c8c1dbb5102b046bf3eb0fd977a52e
btsutskiridze <93125681+btsutskiridze@users.noreply.github.com>
2022-02-26 19:21:20 +0400

    fiirst commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const countryParts = document.querySelectorAll(".country-parts");
+const svg = document.querySelector("svg");
+const path = svg.lastElementChild.children;
+
+for (let i = 0; i < countryParts.length; i++) {
+  path[i].addEventListener("mouseover", function () {
+    countryParts[i].style.display = "block";
+  });
+  path[i].addEventListener("mouseout", function () {
+    countryParts[i].style.display = "none";
+  });
+}
+
+const mapBtn = document.getElementById("map-btn");
+const welcomePage = document.querySelector(".welcome-container");
+const mapDiv = document.querySelector(".map-div");
+mapBtn.addEventListener("click", function () {
+  welcomePage.style.display = "none";
+  mapDiv.style.display = "block";
+});
